afccd57603811660bfac05627d6382126f8ef4f5
Jared Schwalbe <jareds94@vt.edu>
2016-04-05 19:12:23 -0400

    BU-3 Added SignIn.xhtml, CreateAccount.xhtml, and MyAccount.xhtml

--- build/generated-sources/ap-source-output/com/mycompany/entitypackage/User_.java
+++ build/generated-sources/ap-source-output/com/mycompany/entitypackage/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T17:56:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T19:11:19")
 @StaticMetamodel(User.class)
 public class User_ { 
 
--- build/generated-sources/ap-source-output/com/mycompany/entitypackage/UsersProgress_.java
+++ build/generated-sources/ap-source-output/com/mycompany/entitypackage/UsersProgress_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T17:56:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T19:11:19")
 @StaticMetamodel(UsersProgress.class)
 public class UsersProgress_ { 
 
