9a34374612831cb6f6d698b6d5e01804605d1954
Jared Schwalbe <jareds94@vt.edu>
2016-04-05 18:00:30 -0400

    Created a new project and added entity classes.

---
+++ build/generated-sources/ap-source-output/com/mycompany/entitypackage/UsersProgress_.java
@@ -0,0 +1,14 @@
+package com.mycompany.entitypackage;
+
+import com.mycompany.entitypackage.UsersProgressPK;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T17:56:39")
+@StaticMetamodel(UsersProgress.class)
+public class UsersProgress_ { 
+
+    public static volatile SingularAttribute<UsersProgress, UsersProgressPK> usersProgressPK;
+
+}
